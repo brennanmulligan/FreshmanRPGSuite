8b5b2ad5ab6ea6478f08077b8baff0121b1ef658
jh8142 <jh8142@cs.ship.edu>
2022-04-20 15:23:47 -0400

    created the VanityAwardsTableDataGateway interface

---
+++ FreshmanRPG/GameServer/src/main/java/datasource/VanityAwardsTableDataGateway.java
@@ -0,0 +1,35 @@
+package datasource;
+
+import dataDTO.VanityDTO;
+
+import java.util.ArrayList;
+
+public interface VanityAwardsTableDataGateway
+{
+    /**
+     * Gets all the vanity awards stored in the database
+     * @return a list of all the vanity awards
+     * @throws DatabaseException shouldn't
+     */
+    ArrayList<VanityDTO> getVanityAwards() throws DatabaseException;
+
+    /**
+     * Adds a vanity award to the vanity awards list so it can be given as a quest reward
+     * @param awardID the ID of the vanity award to add
+     * @throws DatabaseException shouldn't
+     */
+    void addVanityAward(int awardID) throws DatabaseException;
+
+    /**
+     * Removes a vanity award from the vanity awards list so it cant be given out anymore
+     * @param awardID the id of the award to be removed
+     * @throws DatabaseException shouldnt
+     */
+    void removeVanityAward(int awardID) throws DatabaseException;
+
+    /**
+     * Resets the data
+     * @throws DatabaseException shouldnt
+     */
+    void resetData() throws DatabaseException;
+}
