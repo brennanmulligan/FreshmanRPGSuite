65a45d0ce568e7c60be002d3fb04942eb7b21f51
jh8142 <jh8142@cs.ship.edu>
2022-04-20 15:23:47 -0400

    created the VanityAwardsTableDataGateway interface

--- FreshmanRPG/GameServer/src/main/java/datasource/VanityAwardsTableDataGateway.java
+++ FreshmanRPG/GameServer/src/main/java/datasource/VanityAwardsTableDataGateway.java
@@ -4,9 +4,6 @@ import dataDTO.VanityDTO;
 
 import java.util.ArrayList;
 
-/**
- * Interface for the vanity awards gateway
- */
 public interface VanityAwardsTableDataGateway
 {
     /**
@@ -17,26 +14,18 @@ public interface VanityAwardsTableDataGateway
     ArrayList<VanityDTO> getVanityAwards() throws DatabaseException;
 
     /**
-     * Gets all the vanity awards for a given questID
-     * @param questID the quest
-     * @return a list of all the vanity awards given by that quest
-     * @throws DatabaseException shouldnt
-     */
-    ArrayList<VanityDTO> getVanityAwardsForQuest(int questID) throws DatabaseException;
-
-    /**
      * Adds a vanity award to the vanity awards list so it can be given as a quest reward
      * @param awardID the ID of the vanity award to add
      * @throws DatabaseException shouldn't
      */
-    void addVanityAward(int questID, int awardID) throws DatabaseException;
+    void addVanityAward(int awardID) throws DatabaseException;
 
     /**
      * Removes a vanity award from the vanity awards list so it cant be given out anymore
      * @param awardID the id of the award to be removed
      * @throws DatabaseException shouldnt
      */
-    void removeVanityAward(int questID, int awardID) throws DatabaseException;
+    void removeVanityAward(int awardID) throws DatabaseException;
 
     /**
      * Resets the data
