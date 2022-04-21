2e54b0effed39ac7e412268634262252a1208c82
aw3076 <aw3076@cs.ship.edu>
2022-04-20 20:02:11 -0400

    FRPG-780 Make testing file for vanity awards mock gateway

---
+++ FreshmanRPG/GameServer/src/test/java/datasource/VanityAwardsTableDataGatewayMockTest.java
@@ -0,0 +1,16 @@
+package datasource;
+
+/**
+ * A class for testing the mock vanity awards table gateway
+ */
+public class VanityAwardsTableDataGatewayMockTest extends VanityAwardsTableDataGatewayTest
+{
+    /**
+     * @return an instance of the mock vanity awards table
+     * @throws DatabaseException
+     */
+    @Override
+    VanityAwardsTableDataGateway findGateway() throws DatabaseException {
+        return VanityAwardsTableDataGatewayMock.getSingleton();
+    }
+}
