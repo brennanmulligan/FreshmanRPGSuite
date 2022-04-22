564b45fed4fb07dc7d9a27d12c4f400d1baadf8f
Andrew Januszko <58179101+andrewjanuszko@users.noreply.github.com>
2022-04-22 10:54:30 -0400

    fixes to network

--- game_manager/lib/main.dart
+++ game_manager/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'features/network/network_provider.dart';
+
 ///
 /// Data for light theme.
 ///
@@ -55,6 +57,6 @@ Future<void> initialize({
   ///
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
-  // container.read(NetworkProvider.networkController.notifier).setBaseURL();
-  // container.refresh(NetworkProvider.serviceClient);
+  container.read(NetworkProvider.networkController.notifier).setBaseURL();
+  container.refresh(NetworkProvider.serviceClient);
 }
