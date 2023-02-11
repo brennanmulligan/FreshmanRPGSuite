8a0a2956880e7f16d5a6898e26c28bba95f22c2b
merlin <merlin@cs.ship.edu>
2023-02-10 23:50:05 -0500

    checkstyle and formatting

--- FreshmanRPG/GameServer/src/main/java/edu/ship/engr/shipsim/restfulcommunication/controllers/PlayerController.java
+++ FreshmanRPG/GameServer/src/main/java/edu/ship/engr/shipsim/restfulcommunication/controllers/PlayerController.java
@@ -20,24 +20,30 @@ public class PlayerController extends Controller
 {
     @CrossOrigin // Required for web client support
     @PostMapping("/player")
-    public ResponseEntity<Object> createPlayer(@RequestBody CreatePlayerInformation info)
+    public ResponseEntity<Object> createPlayer(
+            @RequestBody CreatePlayerInformation info)
     {
         CreatePlayerResponseReport report = processAction(() ->
         {
-            CommandCreatePlayer command = new CommandCreatePlayer(info.getUsername(), info.getPassword(),
-                    info.getCrewNum(), info.getMajorNum(), info.getSection());
+            CommandCreatePlayer command =
+                    new CommandCreatePlayer(info.getUsername(),
+                            info.getPassword(),
+                            info.getCrewNum(), info.getMajorNum(),
+                            info.getSection());
             ModelFacade.getSingleton().queueCommand(command);
         }, CreatePlayerResponseReport.class);
 
         if (report != null)
         {
-            if(report.isSuccessful())
+            if (report.isSuccessful())
             {
                 return new ResponseEntity<>(HttpStatus.OK);
             }
             else
             {
-                return new ResponseEntity<>(new GeneralFailureResponse(report.getDescription()), HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(
+                        new GeneralFailureResponse(report.getDescription()),
+                        HttpStatus.BAD_REQUEST);
             }
         }
 
