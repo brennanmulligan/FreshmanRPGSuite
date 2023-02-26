976ef862fca5e9a6397b45d41935d40703e517ab
Merlin <merlin@cs.ship.edu>
2023-02-26 22:40:34 +0000

    Bloc testing

---
+++ game_manager/lib/type_definitions.dart
@@ -0,0 +1,9 @@
+///
+/// Alias `Map<String, dynamic>` as `JSON`.
+///
+typedef JSON = Map<String, dynamic>;
+
+///
+/// Alias `Map<String, String>` as `HEADER`.
+///
+typedef HEADER = Map<String, String>;
