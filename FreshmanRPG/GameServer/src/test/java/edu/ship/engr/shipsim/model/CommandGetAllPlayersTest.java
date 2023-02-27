78a29f9fadeee6d35e98e85854d7052cc2cf03e0
Scott Bucher <sb2490@engr.ship.edu>
2023-02-27 16:42:15 -0500

    Added get all players command test

--- FreshmanRPG/GameServer/src/test/java/edu/ship/engr/shipsim/model/CommandGetAllPlayersTest.java
+++ FreshmanRPG/GameServer/src/test/java/edu/ship/engr/shipsim/model/CommandGetAllPlayersTest.java
@@ -1,2 +1,48 @@
-package edu.ship.engr.shipsim.model;public class CommandGetAllPlayersTest {
+package edu.ship.engr.shipsim.model;
+
+import edu.ship.engr.shipsim.dataDTO.PlayerDTO;
+import edu.ship.engr.shipsim.datasource.DatabaseException;
+import edu.ship.engr.shipsim.datasource.PlayerLoginRowDataGateway;
+import edu.ship.engr.shipsim.datasource.PlayerTableDataGateway;
+import edu.ship.engr.shipsim.model.reports.CreatePlayerResponseReport;
+import edu.ship.engr.shipsim.model.reports.GetAllPlayersReport;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class CommandGetAllPlayersTest
+{
+    @Test
+    public void testGetAllPlayers() throws DatabaseException
+    {
+        ReportObserver obs = mock(ReportObserver.class);
+        ReportObserverConnector.getSingleton().registerObserver(obs, GetAllPlayersReport.class);
+
+        PlayerTableDataGateway gateway = PlayerTableDataGateway.getSingleton();
+        PlayerTableDataGateway mockGateway = mock(PlayerTableDataGateway.class);
+        gateway.setSingleton(mockGateway);
+
+        PlayerDTO player1 = new PlayerDTO();
+        player1.setPlayerID(1);
+        PlayerDTO player2 = new PlayerDTO();
+        player2.setPlayerID(2);
+
+        ArrayList<PlayerDTO> players = new ArrayList<>() {{
+            add(player1);
+            add(player2);
+        }};
+
+        when(mockGateway.retrieveAllPlayers()).thenReturn(
+                players);
+
+        CommandGetAllPlayers cmd = new CommandGetAllPlayers();
+        cmd.execute();
+
+        verify(obs, times(1)).receiveReport(new GetAllPlayersReport(players));
+    }
 }
