833cffaf9e7e3d1c5a81349205ccb63b2d532423
Scott Bucher <sb2490@engr.ship.edu>
2023-02-26 22:21:23 -0500

    Added testing for new get all players method and renamed old tests to be more specific

--- FreshmanRPG/GameServer/src/test/java/edu/ship/engr/shipsim/restfulcommunication/controllers/TestPlayerController.java
+++ FreshmanRPG/GameServer/src/test/java/edu/ship/engr/shipsim/restfulcommunication/controllers/TestPlayerController.java
@@ -1,6 +1,8 @@
 package edu.ship.engr.shipsim.restfulcommunication.controllers;
 
+import edu.ship.engr.shipsim.dataDTO.PlayerDTO;
 import edu.ship.engr.shipsim.model.reports.CreatePlayerResponseReport;
+import edu.ship.engr.shipsim.model.reports.GetAllPlayersReport;
 import edu.ship.engr.shipsim.restfulcommunication.representation.CreatePlayerInformation;
 import edu.ship.engr.shipsim.testing.annotations.GameTest;
 import edu.ship.engr.shipsim.testing.annotations.ResetModelFacade;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,7 +34,7 @@ public class TestPlayerController
 {
 
     @Test
-    public void goodResponse()
+    public void goodResponseCreatePlayer()
     {
         PlayerController mock = mock(PlayerController.class);
         when(mock.processAction(any(Runnable.class), eq(CreatePlayerResponseReport.class))).thenReturn(
@@ -42,11 +45,10 @@ public class TestPlayerController
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("{\"description\":\"Created\",\"success\":true}", Objects.requireNonNull(response.getBody()).toString());
-
     }
 
     @Test
-    public void badResponse()
+    public void badResponseCreatePlayer()
     {
         PlayerController mock = mock(PlayerController.class);
         when(mock.processAction(any(Runnable.class), eq(CreatePlayerResponseReport.class))).thenReturn(
@@ -56,6 +58,27 @@ public class TestPlayerController
         ResponseEntity<Object> response = mock.createPlayer(new CreatePlayerInformation("fred", "ow", 1, 2, 3));
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
         assertEquals("{\"description\":\"ERROR\",\"success\":false}", Objects.requireNonNull(response.getBody()).toString());
+    }
+
+    @Test
+    public void goodResponseGetAllPlayers()
+    {
+        PlayerController mock = mock(PlayerController.class);
+
+        when(mock.processAction(any(Runnable.class), eq(GetAllPlayersReport.class))).thenReturn(
+                new GetAllPlayersReport(new ArrayList<>() {{
+                    add(mock(PlayerDTO.class));
+                    add(mock(PlayerDTO.class));
+                    add(mock(PlayerDTO.class));
+                }}));
+
+        when(mock.getAllPlayers()).thenCallRealMethod();
+
+        ResponseEntity<Object> response = mock.getAllPlayers();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+//        assertEquals("[{\"id\":0,\"username\":\"\",\"password\":\"\",\"crewNum\":0,\"majorNum\":0,\"section\":0,\"questId\":0,\"questProgress\":0,\"questCompleted\":false,\"questFailed\":false},{\"id\":0,\"username\":\"\",\"password\":\"\",\"crewNum\":0,\"majorNum\":0,\"section\":0,\"questId\":0,\"questProgress\":0,\"questCompleted\":false,\"questFailed\":false},{\"id\":0,\"username\":\"\",\"password\":\"\",\"crewNum\":0,\"majorNum\":0,\"section\":0,\"questId\":0,\"questProgress\":0,\"questCompleted\":false,\"questFailed\":false}]",
+//                Objects.requireNonNull(response.getBody()).toString());
     }
 }
 
