43e07f6cc2ae96f590fef4a6f4c3c2e8bc087290
Bobby Tables <ns7154@engr.ship.edu>
2023-03-01 16:42:47 -0500

    Separated check password into its own class

--- FreshmanRPG/GameServer/src/main/java/edu/ship/engr/shipsim/model/CommandCreatePlayer.java
+++ FreshmanRPG/GameServer/src/main/java/edu/ship/engr/shipsim/model/CommandCreatePlayer.java
@@ -30,7 +30,8 @@ public class CommandCreatePlayer extends Command
         this.major = Major.getMajorForID(majorNum);
         this.section = section;
 
-        if(!checkPassword())
+        CheckPassword checkPwd = new CheckPassword();
+        if(!checkPwd.checkPassword(password))
         {
             ReportObserverConnector.getSingleton().sendReport(new CreatePlayerResponseReport(false, "Bad Password"));
         }
@@ -58,68 +59,28 @@ public class CommandCreatePlayer extends Command
     }
 
     private void triggerInitialQuests(int playerID) throws DatabaseException
-{
-    QuestStateTableDataGateway questStateTableDataGatewayRDS =
-            QuestStateTableDataGateway.getSingleton();
-    ObjectiveStateTableDataGateway objectiveStateTableDataGateway =
-            ObjectiveStateTableDataGateway.getSingleton();
-    ObjectiveTableDataGateway objectiveTableDataGateway =
-            ObjectiveTableDataGateway.getSingleton();
-
-    for (QuestsForProduction q : questsToTrigger)
     {
-        questStateTableDataGatewayRDS.updateState(playerID, q.getQuestID(),
-                QuestStateEnum.TRIGGERED, true);
+        QuestStateTableDataGateway questStateTableDataGatewayRDS =
+                QuestStateTableDataGateway.getSingleton();
+        ObjectiveStateTableDataGateway objectiveStateTableDataGateway =
+                ObjectiveStateTableDataGateway.getSingleton();
+        ObjectiveTableDataGateway objectiveTableDataGateway =
+                ObjectiveTableDataGateway.getSingleton();
 
-        //Add relevant Objectives
-        ArrayList<ObjectiveRecord> objectiveList =
-                objectiveTableDataGateway.getObjectivesForQuest(q.getQuestID());
-        for (ObjectiveRecord objective : objectiveList)
+        for (QuestsForProduction q : questsToTrigger)
         {
-            objectiveStateTableDataGateway.updateState(playerID, q.getQuestID(),
-                    objective.getObjectiveID(), ObjectiveStateEnum.TRIGGERED,
-                    false);
+            questStateTableDataGatewayRDS.updateState(playerID, q.getQuestID(),
+                    QuestStateEnum.TRIGGERED, true);
+
+            //Add relevant Objectives
+            ArrayList<ObjectiveRecord> objectiveList =
+                    objectiveTableDataGateway.getObjectivesForQuest(q.getQuestID());
+            for (ObjectiveRecord objective : objectiveList)
+            {
+                objectiveStateTableDataGateway.updateState(playerID, q.getQuestID(),
+                        objective.getObjectiveID(), ObjectiveStateEnum.TRIGGERED,
+                        false);
+            }
         }
     }
-}
-
-    private boolean checkPassword()
-    {
-        /**
-         * Password Requirements
-         * 8-16 characters in length
-         * 1+ capital letters
-         * 1+ lowercase letters
-         * 1+ special characters
-         */
-
-        // 8 - 16 char length
-        if(password.length() < 8 || password.length() > 16)
-        {
-                return false;
-        }
-
-        // Has capital
-        char current;
-        boolean hasCapital = false, hasLowercase = false, hasSpecial = false;
-        for(int i = 0; i < password.length(); i++)
-        {
-            current = password.charAt(i);
-            if(Character.isUpperCase(current))
-            {
-                hasCapital = true;
-            }
-            else if(Character.isLowerCase(current))
-            {
-                hasLowercase = true;
-            }
-            else
-            {
-                hasSpecial = true;
-            }
-        }
-
-        return (hasCapital && hasLowercase && hasSpecial);
-    }
-
 }
