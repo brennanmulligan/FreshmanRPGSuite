107a6a05eb4b1bed23d0990d96224bb0f155590d
Jeray Neely <jn3557@engr.ship.edu>
2023-03-01 16:33:27 -0500

    update event class

--- game_manager/lib/pages/get_majors_and_crews/bloc/create_majors_crews_event.dart
+++ game_manager/lib/pages/get_majors_and_crews/bloc/create_majors_crews_event.dart
@@ -1,4 +1,6 @@
-
+/*
+ creates a majors and crews event class
+ */
 abstract class CreateMajorsCrewsEvent{}
 
 class SendCreateMajorsCrewEvent extends CreateMajorsCrewsEvent {
@@ -7,8 +9,10 @@ class SendCreateMajorsCrewEvent extends CreateMajorsCrewsEvent {
     final num  majors;
 
 
-  SendCreateMajorsCrewEvent(this.crews, this.majors);
-
+    SendCreateMajorsCrewEvent(this.crews, this.majors);
+/*
+returns the major and crews event string
+ */
   String toSting() {
     return 'SendCreateMajorsCrewsEvent(crew: $crews, major: $majors)';
   }
