ffc25bf05b577ea1cf35bb6aa6bfe796683e79bc
Brennan Mulligan <bm4916@engr.ship.edu>
2023-03-01 16:24:35 -0500

    Began fixing the execute method to properly get from database

--- FreshmanRPG/GameServer/src/main/java/edu/ship/engr/shipsim/model/CommandGetMajorsCrews.java
+++ FreshmanRPG/GameServer/src/main/java/edu/ship/engr/shipsim/model/CommandGetMajorsCrews.java
@@ -1,30 +1,70 @@
 package edu.ship.engr.shipsim.model;
 
 import edu.ship.engr.shipsim.datasource.DatabaseException;
+import edu.ship.engr.shipsim.datasource.DatabaseManager;
+import edu.ship.engr.shipsim.datasource.PlayerRowDataGateway;
 import edu.ship.engr.shipsim.datatypes.Crew;
 import edu.ship.engr.shipsim.datatypes.Major;
-import edu.ship.engr.shipsim.restfulcommunication.controllers.PlayerController;
+import edu.ship.engr.shipsim.
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class CommandGetMajorsCrews extends Command
 {
-    private Major[] majors;
-    private Crew[] crews;
+    private int playerID;
 
     /**
      *
-     * @param majors the majors
-     * @param crews the crews
+     * @param playerID the player's id
      */
-    public CommandGetMajorsCrews(Major[] majors, Crew[] crews) {
-        this.majors = majors;
-        this.crews = crews;
+    public CommandGetMajorsCrews() {
     }
     @Override
     void execute()
     {
         try
         {
-            PlayerController.getMajorsCrews(majors, crews);
+            Connection connection = DatabaseManager.getSingleton().getConnection();
+            try (PreparedStatement stmt = connection.prepareStatement(
+                    "Insert INTO Players SET appearanceType = ?, quizScore = ?, experiencePoints = ?, crew = ?, major = ?, section = ?, buffPool = ?, online = ?",
+                    Statement.RETURN_GENERATED_KEYS))
+            {
+                stmt.setString(1, appearanceType);
+                stmt.setInt(2, quizScore);
+                stmt.setInt(3, experiencePoints);
+                stmt.setInt(4, crew.getID());
+                stmt.setInt(5, major.getID());
+                stmt.setInt(6, section);
+                stmt.setInt(7, buffPool);
+                stmt.setBoolean(8, online);
+                stmt.executeUpdate();
+
+                try (ResultSet rs = stmt.getGeneratedKeys())
+                {
+                    if (rs.next())
+                    {
+                        playerID = rs.getInt(1);
+                        this.appearanceType = appearanceType;
+                        this.quizScore = quizScore;
+                        this.experiencePoints = experiencePoints;
+
+                        this.connection = connection;
+                        this.crew = crew;
+                        this.major = major;
+                        this.section = section;
+
+                        this.buffPool = 0;
+                        this.online = online;
+                    }
+                    else
+                    {
+                        throw new DatabaseException(
+                                "Generated key for player not found.");
+                    }
+                }
         }
         catch (DatabaseException e)
         {
