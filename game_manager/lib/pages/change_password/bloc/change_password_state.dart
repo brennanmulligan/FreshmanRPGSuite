0df30728a3e04167621ed052afc07843648b7cfb
Scott Bucher <sb2490@engr.ship.edu>
2023-03-13 20:17:43 +0000

    Resolve "Create change_password_state class"

--- game_manager/lib/pages/change_password/bloc/change_password_state.dart
+++ game_manager/lib/pages/change_password/bloc/change_password_state.dart
@@ -1,6 +1,22 @@
 part of 'change_password_bloc.dart';
 
+/// The states that the ChangePasswordBloc can be in.
+
 @immutable
-abstract class ChangePasswordState {}
+abstract class ChangePasswordState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
 
 class ChangePasswordInitial extends ChangePasswordState {}
+
+class ChangePasswordLoading extends ChangePasswordState {}
+
+class ChangePasswordComplete extends ChangePasswordState {
+  final BasicResponse response;
+
+  ChangePasswordComplete(this.response);
+
+  @override
+  List<Object> get props => [response];
+}
