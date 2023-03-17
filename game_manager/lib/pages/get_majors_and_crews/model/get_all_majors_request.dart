8ebc727c800e01ca10dd6dc65fc4ca174080412d
Taylor Fernandez <tf1740@ship.edu>
2023-03-17 22:51:25 +0000

    Resolve "Request class for getting majors and crews"

---
+++ game_manager/lib/pages/get_majors_and_crews/model/get_all_majors_request.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+import '../../../type_definitions.dart';
+
+class GetAllMajorsRequest extends Equatable{
+
+  const GetAllMajorsRequest();
+
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+
+  Map<String, dynamic> toJson() {
+    return {
+    };
+  }
+
+  @override
+  String toString() {
+    return 'GetAllMajorsRequest()';
+  }
+
+  JSON get asJson => {
+  };
+}
