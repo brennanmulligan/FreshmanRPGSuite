c918f321957f68712fdc6743983e1aad7b982b0c
Jeray Neely <jn3557@engr.ship.edu>
2023-03-17 13:19:05 -0400

    fixing errors

---
+++ game_manager/lib/pages/get_majors_and_crews/bloc/get_majors_and_crews_state.dart
@@ -0,0 +1,20 @@
+part of 'get_majors_and_crews_bloc.dart';
+
+@immutable
+abstract class GetMajorsAndCrewsState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class GetMajorsAndCrewsInitial extends GetMajorsAndCrewsState {}
+
+class GetMajorsAndCrewsLoading extends GetMajorsAndCrewsState {}
+
+class GetMajorCrewComplete extends GetMajorsAndCrewsState {
+  final BasicResponse response;
+
+  GetMajorCrewComplete(this.response);
+
+  @override
+  List<Object> get props => [response];
+}
