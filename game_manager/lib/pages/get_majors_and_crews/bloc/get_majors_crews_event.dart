da3073dc92fe6cd7e4f37225b38a7fa361013e7f
Jeray Neely <jn3557@engr.ship.edu>
2023-03-17 13:19:05 -0400

    fixing errors

---
+++ game_manager/lib/pages/get_majors_and_crews/bloc/get_majors_crews_event.dart
@@ -0,0 +1,23 @@
+/*
+ creates a majors and crews event class
+ /
+
+ */
+part of 'get_majors_and_crews_bloc.dart';
+abstract class GetMajorsAndCrewsEvent{}
+
+class SendCreateMajorsCrewsEvent extends GetMajorsAndCrewsEvent {
+
+    final num crews;
+    final num  majors;
+
+
+    SendCreateMajorsCrewsEvent(this.crews, this.majors);
+/*
+returns the major and crews event string
+ */
+
+String toSting() {
+  return 'SendCreateMajorsCrewsEvent(crew: $crews, major: $majors)';
+}
+}
