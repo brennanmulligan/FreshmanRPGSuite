85b8ea838f3d2b470d341fc2318939245b7ed251
Jeray Neely <jn3557@engr.ship.edu>
2023-03-17 13:19:05 -0400

    fixing errors

---
+++ game_manager/lib/pages/get_majors_and_crews/bloc/get_majors_and_crews_bloc.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:game_manager/repository/player/basic_response.dart';
+
+part 'get_majors_and_crews_event.dart';
+part 'get_majors_and_crews_state.dart';
+
+class GetMajorsAndCrewsBloc extends Bloc<GetMajorsAndCrewsEvent, GetMajorsAndCrewsState> {
+  late GetMajorsAndCrewsRequest data;
+  final GetMajorsAndCrewsRepository;
+
+  GetMajorsAndCrewsBloc({
+    required this.GetMajorsAndCrewsRepository
+  }) : super(GetMajorsAndCrewsInitial()) {
+    on<SendCreateMajorsCrewsEvent>((event, emit) async {
+      emit(GetMajorsAndCrewsLoading());
+      GetMajorsAndCrewsResponse response = await GetMajorsAndCrewsRepository.getMajorsAndCrews();
+
+      if (response.success) {
+        emit(GetMajorsAndCrewsSuccess(response));
+      } else {
+        emit(GetMajorsAndCrewsFail());
+      }
+    });
+  }
+}
