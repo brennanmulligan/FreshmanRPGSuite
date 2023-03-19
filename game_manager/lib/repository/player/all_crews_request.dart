e2e2752818655cb02d5740815c05b39adce39f79
Scott Bucher <sb2490@engr.ship.edu>
2023-03-19 21:36:08 +0000

    Resolve "Repository for majors and crews that calls the respective endpoints for getting all majors and crews"

---
+++ game_manager/lib/repository/player/all_crews_request.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class AllCrewsRequest extends Equatable {
+
+  const AllCrewsRequest();
+
+  @override
+  List<Object?> get props => [];
+
+  Map<String, dynamic> toJson() {
+    return {};
+  }
+
+  @override
+  String toString() {
+    return 'AllObjectivesRequest()';
+  }
+}
