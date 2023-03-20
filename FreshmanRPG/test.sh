ed271022844f6d588c2811c2488ffe91621a4092
Derek Williams <MrFrydae@gmail.com>
2023-03-20 12:37:37 -0400

    update test script

--- FreshmanRPG/test.sh
+++ FreshmanRPG/test.sh
@@ -7,31 +7,33 @@ GREEN='\033[0;32m'
 WHITE='\033[0;37m'
 RESET='\033[0m'
 
-# array containing each module name
+lastModule=0
+printedTail=0
+
 declare -a modules=("GameShared" "GameClient" "GameClient-desktop" "GameSequenceTests" "GameServer" "LoginServer")
 declare -a tests=("edu.ship.engr.shipsim.AllSharedTests" "edu.ship.engr.shipsim.AllClientTests" "" "edu.ship.engr.shipsim.model.RunAllSequenceTests" "edu.ship.engr.shipsim.AllServerTests" "edu.ship.engr.shipsim.AllLoginServerTests")
 
-# 30 cell array originally containing "waiting"
 declare -a table
-for (( i=0; i<=30; i++ )); do
+for (( i=0; i<=18; i++ )); do
     table[$i]="waiting"
 done
 
-# function that prints calculates an index for two coordinates, then prints the value at that index in the table array
+# function that converts an index to a cell coordinate
+function indexToCell() {
+    local index=$1
+
+    local x=$(( ( index - 1 ) / 3 + 1 ))
+    local y=$(( ( index - 1 ) % 3 + 1 ))
+
+    echo "$x $y"
+}
+
 function printCell() {
     local x=$1
     local y=$2
 
     local index=$(( ( ( x - 1 ) * 3 ) + y ))
 
-
-    # if the cell is waiting, print it in white
-    # if the cell is running, print it in blue
-    # if the cell is skipped, print it in blue
-    # if the cell is complete, print it in green
-    # if the cell is failed, print it in red
-    # reset color after printing
-    # print it all in one line
     if [[ ${table[$index]} == "waiting" ]]; then
         printf "${WHITE}%s${RESET}" "${table[$index]}"
     elif [[ ${table[$index]} == "running" ]]; then
@@ -45,35 +47,62 @@ function printCell() {
     fi
 }
 
-# function that prints a 2x10 table, with each cell running the printRunning function
-function printTable() {
-    # move cursor to top left of terminal
-    echo -ne "\033[0;0H"
+# function that prints table bars given a specific column position
+function printBars() {
+    local column=$1
 
-    printf "┌────────────────────┬──────────┬────────────┬──────────┐\n"
-    printf "│    Module Name     │ Compile  │ Checkstyle │  Testing │\n"
     printf "├────────────────────┼──────────┼────────────┼──────────┤\n"
-    printf "│     GameShared     │ %19s │ %21s │ %19s │\n" "$(printCell 1 1)" "$(printCell 1 2)" "$(printCell 1 3)"
-    printf "│     GameClient     │ %19s │ %21s │ %19s │\n" "$(printCell 2 1)" "$(printCell 2 2)" "$(printCell 2 3)"
-    printf "│ GameClient-desktop │ %19s │ %21s │ %19s │\n" "$(printCell 3 1)" "$(printCell 3 2)" "$(printCell 3 3)"
-    printf "│  GameSequenceTests │ %19s │ %21s │ %19s │\n" "$(printCell 4 1)" "$(printCell 4 2)" "$(printCell 4 3)"
-    printf "│      GameServer    │ %19s │ %21s │ %19s │\n" "$(printCell 5 1)" "$(printCell 5 2)" "$(printCell 5 3)"
-    printf "│      LoginServer   │ %19s │ %21s │ %19s │\n" "$(printCell 6 1)" "$(printCell 6 2)" "$(printCell 6 3)"
+    printf "│     GameShared     │ %19s │ %21s │ %19s │\n" "$(printCell 1 $column)" "$(printCell 1 $(( column + 1 )))" "$(printCell 1 $(( column + 2 )))"
+    printf "│     GameClient     │ %19s │ %21s │ %19s │\n" "$(printCell 2 $column)" "$(printCell 2 $(( column + 1 )))" "$(printCell 2 $(( column + 2 )))"
+    printf "│ GameClient-desktop │ %19s │ %21s │ %19s │\n" "$(printCell 3 $column)" "$(printCell 3 $(( column + 1 )))" "$(printCell 3 $(( column + 2 )))"
+    printf "│  GameSequenceTests │ %19s │ %21s │ %19s │\n" "$(printCell 4 $column)" "$(printCell 4 $(( column + 1 )))" "$(printCell 4 $(( column + 2 )))"
+    printf "│      GameServer    │ %19s │ %21s │ %19s │\n" "$(printCell 5 $column)" "$(printCell 5 $(( column + 1 )))" "$(printCell 5 $(( column + 2 )))"
+    printf "│     LoginServer    │ %19s │ %21s │ %19s │\n" "$(printCell 6 $column)" "$(printCell 6 $(( column + 1 )))" "$(printCell 6 $(( column + 2 )))"
+}
 
-    # print footer separator
-    echo "└────────────────────┴──────────┴────────────┴──────────┘"
+# array with module names centered with spaces
+modulesDisplay=( "    GameShared    " "    GameClient    " "GameClient-desktop" " GameSequenceTests" "     GameServer   " "    LoginServer   " )
+
+function printElement() {
+    local row=$1
+    local column=$2
+
+    # if column is 1
+    if [[ $column == 1 ]]; then
+        printf "│ %s │ %19s │" "${modulesDisplay[$(( row - 1 ))]}" "$(printCell "${row}" "${column}")"
+    fi
+
+    # if column is 2
+    if [[ $column == 2 ]]; then
+        printf " %21s │" "$(printCell "${row}" "${column}")"
+    fi
+
+    # if column is 3
+    if [[ $column == 3 ]]; then
+        printf " %19s │\n" "$(printCell "${row}" "${column}")"
+    fi
+}
+
+function printRemainder() {
+    for (( i=lastModule; i<=18; i++ )); do
+        # variables for row and column
+        local row=$(( ( i - 1 ) / 3 + 1 ))
+        local column=$(( ( i - 1 ) % 3 + 1 ))
+
+        printElement $row $column
+    done
+
+    # if last module is not 18
+    if [[ $lastModule != 18 ]]; then
+        printf "└────────────────────┴──────────┴────────────┴──────────┘\n"
+        printedTail=1
+    fi
 }
 
 function runTask() {
     # local variable for table index
     local index=$(( ( ( $1 - 1 ) * 3 ) + $2 ))
     local i=$1
-
-    # set table cell to running
-    table[$index]="running"
-
-    # print table
-    printTable
 
     if [[ $2 == 1 || $2 == 2 ]]; then
         if [[ ${modules[$i-1]} == "GameClient-desktop" ]]; then
@@ -90,9 +119,19 @@ function runTask() {
         fi
     fi
 
-    if [[ $? -ne 0 ]]; then
+    rc=$?
+
+    lastModule=$index
+
+    if [[ $rc -ne 0 ]]; then
         table[$index]="failed"
-        printTable
+
+        # set remaining cells to skipped
+        for (( i=$index+1; i<=18; i++ )); do
+            table[$i]="skipped"
+        done
+
+        printRemainder
 
         printf "%s\n" "$output"
 
@@ -104,19 +143,28 @@ function runTask() {
         table[$index]="complete"
     fi
 
-    printTable
+    # print element if skipped or complete
+    if [[ ${table[$index]} == "skipped" || ${table[$index]} == "complete" ]]; then
+        printElement $1 $2
+    fi
 
     return 0
 }
 
 function run() {
-    printTable
+    printf "┌────────────────────┬──────────┬────────────┬──────────┐\n"
+    printf "│    Module Name     │ Compile  │ Checkstyle │  Testing │\n"
+    printf "├────────────────────┼──────────┼────────────┼──────────┤\n"
 
     for (( i=1; i<=6; i++ )); do
         runTask "$i" 1 compileJava compileTestJava || break
         runTask "$i" 2 checkstyleMain checkstyleTest || break
         runTask "$i" 3 test || break
     done
+
+    if [[ $printedTail -eq 0 ]]; then
+        printf "└────────────────────┴──────────┴────────────┴──────────┘\n"
+    fi
 }
 
 function seedServer() {
@@ -143,10 +191,9 @@ function seedServer2() {
     cd - > /dev/null || exit
 }
 
-seedServer 2>&1 /dev/null || exit
-seedLogin 2>&1 /dev/null || exit
-seedShared 2>&1 /dev/null || exit
-seedServer2 2>&1 /dev/null || exit
+seedServer || exit
+seedLogin || exit
+seedShared || exit
+seedServer2 || exit
 
-clear
 run
