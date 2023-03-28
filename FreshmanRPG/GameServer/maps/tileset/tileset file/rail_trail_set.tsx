1cdd7532ce50e3f86a61df31f51b90d766425751
Gregory Neibert <gn3092@engr.ship.edu>
2023-03-28 02:03:12 +0000

    Modified the outsideOfMowrey map and added teleportation between Fountain, quad, outsideOfMowrey, and Rail Trail

---
+++ FreshmanRPG/GameServer/maps/tileset/tileset file/rail_trail_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="rail_trail_set" tilewidth="32" tileheight="32" tilecount="72" columns="12">
+ <image source="Documents/College/Spring2023/SWE420/gameDev/freshmanrpgsuite/FreshmanRPG/GameClient-desktop/maps/tileset/grass-tiles-2-small.png" width="384" height="192"/>
+</tileset>
