a7d0a76d99d926e47b3c7c12ee60b96b9f49ad16
Seth Miller <sm3136@engr.ship.edu>
2023-03-29 02:45:31 +0000

    Map teleportation and fixes

---
+++ FreshmanRPG/GameClient-desktop/maps/tileset/functionalTileSets/indoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="indoor" tilewidth="32" tileheight="32" tilecount="2176" columns="8">
+ <image source="../../../../GameClient-desktop/maps/tileset/Inq XP MI- Medieval Indoors.png" width="256" height="8704"/>
+</tileset>
---
+++ FreshmanRPG/GameClient-desktop/maps/tileset/tileset file/FoutainWater.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="FoutainWater" tilewidth="32" tileheight="32" tilecount="4176" columns="8">
+ <image source="../Projects/freshmanrpgsuite/FreshmanRPG/GameClient-desktop/maps/tileset/Inq XP MT- Medieval Town.png" width="256" height="16704"/>
+</tileset>
