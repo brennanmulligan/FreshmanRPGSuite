30fbbbf4ab25a38dc97002a80a5af1c5e68daac3
Seth Miller <sm3136@engr.ship.edu>
2023-03-29 02:45:31 +0000

    Map teleportation and fixes

---
+++ FreshmanRPG/GameClient-desktop/maps/tileset/functionalTileSets/door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="door" tilewidth="32" tileheight="32" tilecount="32" columns="4">
+ <image source="../../../../GameClient-desktop/maps/tileset/Inq XP Doors.png" width="128" height="256"/>
+</tileset>
---
+++ FreshmanRPG/GameClient-desktop/maps/tileset/tileset file/FountainMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="FountainMap" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="../Projects/freshmanrpgsuite/FreshmanRPG/GameClient-desktop/maps/tileset/Inq XP MT - Dirt.png" width="96" height="128"/>
+</tileset>
