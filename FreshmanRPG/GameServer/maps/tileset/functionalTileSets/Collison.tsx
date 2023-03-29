7dcaeebc4ac6a02fa22c3973a6b28f879aa6fd8a
Seth Miller <sm3136@engr.ship.edu>
2023-03-29 02:45:31 +0000

    Map teleportation and fixes

---
+++ FreshmanRPG/GameServer/maps/tileset/functionalTileSets/Collison.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Collison" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="../../../../GameClient-desktop/maps/tileset/collision.png" width="64" height="32"/>
+</tileset>
---
+++ FreshmanRPG/GameServer/maps/tileset/tileset file/Collision_layer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Collision_layer" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="../../tileset/collision.png" width="64" height="32"/>
+</tileset>
