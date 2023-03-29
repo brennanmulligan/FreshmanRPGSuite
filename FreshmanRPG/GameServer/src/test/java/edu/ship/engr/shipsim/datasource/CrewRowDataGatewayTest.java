2f6f7fad76fd2872869bea35ff39d9bbdc666b73
Badreddine Bouhmam <bb6848@engr.ship.edu>
2023-03-29 20:09:07 +0000

    Resolve "Crews Gateway class with CRUD functionality"

---
+++ FreshmanRPG/GameServer/src/test/java/edu/ship/engr/shipsim/datasource/CrewRowDataGatewayTest.java
@@ -0,0 +1,74 @@
+package edu.ship.engr.shipsim.datasource;
+
+import edu.ship.engr.shipsim.testing.annotations.GameTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for crews row data gateway
+ */
+@GameTest("GameServer")
+public class CrewRowDataGatewayTest
+{
+    /**
+     * Creates the table in the database
+     * @throws DatabaseException
+     */
+    @Test
+    public void createTable() throws DatabaseException
+    {
+        CrewRowDataGateway.creatTable();
+    }
+
+    /**
+     * Tests in we can create a new crew in the databse
+     * @throws DatabaseException
+     */
+    @Test
+    public void createCrewTest() throws DatabaseException
+    {
+        CrewRowDataGateway gateway = new CrewRowDataGateway(1, "OffByOne");
+        String name = new CrewRowDataGateway(1).getName();
+        assertEquals(name, gateway.getName());
+    }
+
+    /**
+     * Tests if we can find a crew from the databse
+     * @throws DatabaseException
+     */
+    @Test
+    public void findCrewTest() throws DatabaseException
+    {
+        CrewRowDataGateway gateway = new CrewRowDataGateway(2, "Forty percent");
+        String name = new CrewRowDataGateway(2).getName();
+        assertEquals(name, gateway.getName());
+    }
+
+    /**
+     * Test if we can update a crew in the database
+     * @throws DatabaseException
+     */
+    @Test
+    public void persistTest() throws DatabaseException
+    {
+        CrewRowDataGateway gateway = new CrewRowDataGateway(3, "NPC");
+        gateway.setName("OutOfBounds");
+        gateway.persist();
+        assertEquals("OutOfBounds", gateway.getName());
+    }
+
+    /**
+     * Tests if we can delete a crew from the database
+     * @throws DatabaseException
+     */
+    @Test
+    public void deleteTest() throws DatabaseException
+    {
+        CrewRowDataGateway gateway = new CrewRowDataGateway(4, "OffByOne");
+        gateway.delete();
+        assertThrows(DatabaseException.class, () ->
+        {
+            CrewRowDataGateway gateway2 = new CrewRowDataGateway(4);
+        });
+    }
+}
