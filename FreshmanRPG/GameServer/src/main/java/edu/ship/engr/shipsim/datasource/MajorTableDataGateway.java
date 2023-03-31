f592fb99d4dc9e050d75498ab0ebf4096b041c5e
Jeray Neely-Speaks <jn3557@engr.ship.edu>
2023-03-31 14:21:53 +0000

    Created Majors row and table gateways for database using CRUD

---
+++ FreshmanRPG/GameServer/src/main/java/edu/ship/engr/shipsim/datasource/MajorTableDataGateway.java
@@ -0,0 +1,91 @@
+package edu.ship.engr.shipsim.datasource;
+
+import edu.ship.engr.shipsim.dataDTO.MajorDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * A table data gateway for crews
+ */
+public class MajorTableDataGateway
+{
+
+    private static MajorTableDataGateway singleton;
+
+    /**
+     * A private method only called by the singleton method
+     */
+    private MajorTableDataGateway()
+    {
+
+    }
+
+    public static MajorTableDataGateway getSingleton()
+    {
+        if (singleton == null)
+        {
+            singleton = new MajorTableDataGateway();
+        }
+        return singleton;
+    }
+
+    /**
+     * For testing
+     * @param instance
+     */
+    public void setSingleton(MajorTableDataGateway instance)
+    {
+        singleton = instance;
+    }
+
+    /**
+     * Retrieve all majors from the database
+     * @return array list of majors DTOs
+     * @throws DatabaseException
+     */
+    public ArrayList<MajorDTO> retrieveAllMajors () throws DatabaseException
+    {
+        Connection connection = DatabaseManager.getSingleton().getConnection();
+
+        try (PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Majors"))
+        {
+            return processResultSetToCrewDTO(stmt);
+        }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("Could not retrieve all majors!", e);
+        }
+    }
+
+    /**
+     * Processes the result set we get back after we execute the query
+     * @param stmt query
+     * @return array list of Major DTOs
+     * @throws DatabaseException
+     */
+    private ArrayList<MajorDTO> processResultSetToCrewDTO(PreparedStatement stmt)
+            throws DatabaseException
+    {
+        ArrayList<MajorDTO> resultList = new ArrayList<>();
+
+        try(ResultSet results = stmt.executeQuery())
+        {
+            while (results.next())
+            {
+                MajorDTO dto = new MajorDTO(results.getInt("majorID"),
+                        results.getString("Name"));
+                resultList.add(dto);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("Couldn't retrieve all majors", e);
+        }
+        return resultList;
+    }
+
+}
