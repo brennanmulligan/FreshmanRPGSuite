d67f318a4310ee12c832100dd7fae26820bd213d
Taylor <tf1740@ship.edu>
2023-04-01 18:40:31 -0400

    Added CreateManyPlayers bloc

--- game_manager/lib/pages/create_many_players/bloc/create_many_players_state.dart
+++ game_manager/lib/pages/create_many_players/bloc/create_many_players_state.dart
@@ -1,1 +1,20 @@
 part of 'create_many_players_bloc.dart';
+
+@immutable
+abstract class CreateManyPlayersState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class CreateManyPlayersInitial extends CreateManyPlayersState {}
+
+class CreateManyPlayersLoading extends CreateManyPlayersState {}
+
+class CreateManyPlayersComplete extends CreateManyPlayersState {
+  late final CreateManyPlayersResponse response;
+
+  CreateManyPlayersComplete(this.response);
+
+  @override
+  List<Object> get props => [response];
+}
