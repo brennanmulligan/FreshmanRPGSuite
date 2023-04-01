b51d5f2938cf405a8a8fe575d893096ac17ab70c
Brennan Mulligan <bm4916@engr.ship.edu>
2023-04-01 23:18:38 +0000

    Resolve "Switch Major and Crews Commands and Tests to Gateway"

--- FreshmanRPG/GameServer/src/test/java/edu/ship/engr/shipsim/model/CommandGetMajorsTest.java
+++ FreshmanRPG/GameServer/src/test/java/edu/ship/engr/shipsim/model/CommandGetMajorsTest.java
@@ -1,6 +1,8 @@
 package edu.ship.engr.shipsim.model;
 
 import edu.ship.engr.shipsim.dataDTO.MajorDTO;
+import edu.ship.engr.shipsim.datasource.DatabaseException;
+import edu.ship.engr.shipsim.datasource.MajorTableDataGateway;
 import edu.ship.engr.shipsim.model.reports.GetAllMajorsReport;
 import org.junit.jupiter.api.Test;
 
@@ -9,24 +11,25 @@ import java.util.ArrayList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class CommandGetMajorsTest
 {
     @Test
-    public void testGetAllMajors()
+    public void testGetAllMajors() throws DatabaseException
     {
         ReportObserver obs = mock(ReportObserver.class);
-        ReportObserverConnector.getSingleton().registerObserver(obs, GetAllMajorsReport.class);
+        ReportObserverConnector.getSingleton().registerObserver(obs,
+                GetAllMajorsReport.class);
 
-        //TODO When the gateways are made next iteration, this will be used.
-        //MajorGateway majorGateway = MajorGateway.getSingleton();
-        //MajorGateway mockMajorGateway = mock(MajorGateway.class);
-        //majorGateway.setSingleton(mockMajorGateway);
+        MajorTableDataGateway majorGateway = MajorTableDataGateway.getSingleton();
+        MajorTableDataGateway mockMajorGateway = mock(MajorTableDataGateway.class);
+        majorGateway.setSingleton(mockMajorGateway);
 
         MajorDTO majorDTO1 = new MajorDTO();
-        majorDTO1.setName("SOFTWARE_ENGINEERING");
+        majorDTO1.setMajorID(1);
         MajorDTO majorDTO2 = new MajorDTO();
-        majorDTO2.setName("ELECTRICAL_ENGINEERING");
+        majorDTO2.setMajorID(2);
 
         ArrayList<MajorDTO> majors = new ArrayList<>()
         {{
@@ -34,13 +37,11 @@ public class CommandGetMajorsTest
             add(majorDTO2);
         }};
 
-        //when(MajorGateway.getAllMajors()).thenReturn(majors);
+        when(mockMajorGateway.retrieveAllMajors()).thenReturn(majors);
 
         CommandGetMajors command = new CommandGetMajors();
         command.execute();
 
-        // See comment at end of file of CommandGetCrewsTest.java, same thing applies here.
-        //verify(obs, times(1)).receiveReport(new GetAllMajorsReport(majors));
-        verify(obs, times(1));
+        verify(obs, times(1)).receiveReport(new GetAllMajorsReport(majors));
     }
 }
