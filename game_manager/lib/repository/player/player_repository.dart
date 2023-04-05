6dc660a8e33d6a3c2cf2c6d3c1d45692bf35653e
Jeray Neely <jn3557@engr.ship.edu>
2023-04-05 00:07:35 -0400

    Creating new Dup name exeception and sending report

--- game_manager/lib/repository/player/player_repository.dart
+++ game_manager/lib/repository/player/player_repository.dart
@@ -5,35 +5,35 @@ import 'change_player_request.dart';
 import 'create_player_request.dart';
 import 'basic_response.dart';
 
-
-
-class PlayerRepository{
-
-  PlayerRepository({required this.dio}){
+class PlayerRepository {
+  PlayerRepository({required this.dio}) {
     dio.options.headers['content-Type'] = 'application/json; charset=UTF-8';
     dio.options.headers['Access-Control-Allow-Origin'] = '*';
     dio.options.baseUrl = "http://127.0.0.1:8080";
   }
   final Dio dio;
 
-  Future<BasicResponse> createPlayer(CreatePlayerRequest request) async
-  {
+  Future<BasicResponse> createPlayer(CreatePlayerRequest request) async {
     print(request);
+
     try {
       final response = await dio.post(
         '/player/create',
-
         data: jsonEncode(request),
       );
+
       return BasicResponse.fromJson(json: jsonDecode(response.data));
     } on DioError catch (e) {
-        return const BasicResponse(success: false, description: "Failed to create player");
+      final errorResponse = BasicResponse.fromJson(
+        json: jsonDecode(e.response?.data ?? '{}'),
+      );
+      String description = errorResponse.description;
+      return BasicResponse(success: false, description: description);
       //TODO Error checking on http response types
     }
   }
 
-  Future<BasicResponse> changePassword(ChangePlayerRequest request) async
-  {
+  Future<BasicResponse> changePassword(ChangePlayerRequest request) async {
     final response = await dio.post(
       '/player/update/',
       data: jsonEncode(request),
