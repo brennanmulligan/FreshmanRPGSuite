53d577760d224c444aeb00e00eaf3e2492456c85
merlin <merlin@cs.ship.edu>
2023-04-09 23:03:08 -0400

    Make commands serializable

--- FreshmanRPG/GameShared/src/main/java/edu/ship/engr/shipsim/model/Command.java
+++ FreshmanRPG/GameShared/src/main/java/edu/ship/engr/shipsim/model/Command.java
@@ -3,11 +3,12 @@ package edu.ship.engr.shipsim.model;
 import edu.ship.engr.shipsim.datasource.DuplicateNameException;
 
 import java.io.IOException;
+import java.io.Serializable;
 
 /**
  * @author merlin
  */
-public abstract class Command implements InfoPacket
+public abstract class Command implements InfoPacket, Serializable
 {
 
     /**
