412dfa4455cd18a782e4846013a7ee8f2abbc1bd
Alecia Meredith <am7011@engr.ship.edu>
2023-04-10 13:09:48 +0000

    Player appearance change report

---
+++ FreshmanRPG/GameServer/src/main/java/edu/ship/engr/shipsim/model/CommandRemovePlayerBike.java
@@ -0,0 +1,32 @@
+package edu.ship.engr.shipsim.model;
+
+import edu.ship.engr.shipsim.dataDTO.VanityDTO;
+import edu.ship.engr.shipsim.datatypes.VanityType;
+import edu.ship.engr.shipsim.model.Command;
+import edu.ship.engr.shipsim.model.Player;
+import edu.ship.engr.shipsim.model.PlayerManager;
+
+import java.util.ArrayList;
+
+public class CommandRemovePlayerBike extends Command
+{
+    private final int playerID;
+
+    CommandRemovePlayerBike(int playerID)
+    {
+        this.playerID = playerID;
+    }
+
+    /**
+     * Removes the bike from the player
+     */
+    @Override
+    void execute()
+    {
+        Player player = PlayerManager.getSingleton().getPlayerFromID(playerID);
+        if (player != null)
+        {
+            player.removeVanityType(VanityType.BIKE);
+        }
+    }
+}
