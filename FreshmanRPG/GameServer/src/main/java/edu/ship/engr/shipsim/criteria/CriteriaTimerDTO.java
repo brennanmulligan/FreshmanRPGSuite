b9bc4ef93b48079913ef5891301c650521829a77
Calvin Niewind <cn6444@cs.ship.edu>
2023-04-10 16:07:35 -0400

    added CriteriaTimerDTO and added a new ObjectiveCompletionType, TIMED

---
+++ FreshmanRPG/GameServer/src/main/java/edu/ship/engr/shipsim/criteria/CriteriaTimerDTO.java
@@ -0,0 +1,40 @@
+package edu.ship.engr.shipsim.criteria;
+
+import edu.ship.engr.shipsim.model.Command;
+
+/**
+ * @author Calvin Niewind,
+ */
+
+public class CriteriaTimerDTO implements ObjectiveCompletionCriteria
+{
+    private long time;
+
+    private long millisecondsPerMinute;
+
+    /**
+     *
+     * @param time - time in minutes, translated to miliseconds
+     * @param command
+     */
+    public void CriteriaIntegerDTO(long time, Command command)
+    {
+        setTime(time * millisecondsPerMinute);
+    }
+
+    private void setTime(long setTime)
+    {
+        time = setTime;
+    }
+
+    public long getTime()
+    {
+        return time;
+    }
+
+    @Override
+    public String toString()
+    {
+        return ("Criteria Timer: " + (time / millisecondsPerMinute));
+    }
+}
