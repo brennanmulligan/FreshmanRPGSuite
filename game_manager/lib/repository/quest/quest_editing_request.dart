94aeeb0456d93c735c39a161689a7debf6456e26
Nicholas Sarian <ns7154@engr.ship.edu>
2023-04-13 00:36:24 +0000

    Resolve "Request / response for getting quest info"

---
+++ game_manager/lib/repository/quest/quest_editing_request.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class QuestRequest extends Equatable {
+
+  const QuestRequest();
+
+  @override
+  List<Object?> get props => [];
+
+  Map<String, dynamic> toJson() {
+    return {};
+  }
+
+  @override
+  String toString() {
+    return 'QuestRequest()';
+  }
+}
