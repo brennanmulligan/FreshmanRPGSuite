92c5713535389dbc71d478cee203972a917ea077
Alex Schreffler <as1857@engr.ship.edu>
2023-04-17 01:57:43 +0000

    Resolve "Create the QuestInformation class in the backend"

---
+++ FreshmanRPG/GameServer/src/main/java/edu/ship/engr/shipsim/restfulcommunication/representation/UpsertQuestInformation.java
@@ -0,0 +1,109 @@
+package edu.ship.engr.shipsim.restfulcommunication.representation;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import edu.ship.engr.shipsim.criteria.QuestCompletionActionParameter;
+import edu.ship.engr.shipsim.dataENUM.QuestCompletionActionType;
+import edu.ship.engr.shipsim.datatypes.Position;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UpsertQuestInformation
+{
+    private final String title;
+    private final String description;
+    private final int xpGained;
+    private final String triggerMapName;
+    private Position position;
+    private final int objectivesForFulfillment;
+    private final QuestCompletionActionType completionActionType;
+    private final Date startDate;
+    private final Date endDate;
+    private final boolean easterEgg;
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public int getXpGained()
+    {
+        return xpGained;
+    }
+
+    public String getTriggerMapName()
+    {
+        return triggerMapName;
+    }
+
+    public Position getPosition()
+    {
+        return position;
+    }
+
+    public int getObjectivesForFulfillment()
+    {
+        return objectivesForFulfillment;
+    }
+
+    public QuestCompletionActionType getCompletionActionType()
+    {
+        return completionActionType;
+    }
+
+    public Date getStartDate()
+    {
+        return startDate;
+    }
+
+    public Date getEndDate()
+    {
+        return endDate;
+    }
+
+    public boolean isEasterEgg()
+    {
+        return easterEgg;
+    }
+
+    @JsonCreator
+    public UpsertQuestInformation(@JsonProperty("title") String title,
+                                  @JsonProperty("description") String description,
+                                  @JsonProperty("xpGained") int xpGained,
+                                  @JsonProperty("triggerMapName") String triggerMapName,
+                                  @JsonProperty("triggerRow") int triggerRow,
+                                  @JsonProperty("triggerCol") int triggerCol,
+                                  @JsonProperty("objectivesForFulfillment") int objectivesForFulfillment,
+                                  @JsonProperty("completionActionType") int completionActionType,
+                                  @JsonProperty("startDate") String startDate,
+                                  @JsonProperty("endDate") String endDate,
+                                  @JsonProperty("easterEgg") boolean easterEgg)
+    {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        this.title = title;
+        this.description = description;
+        this.xpGained = xpGained;
+        this.triggerMapName = triggerMapName;
+        this.position = new Position(triggerRow, triggerCol);
+        this.objectivesForFulfillment = objectivesForFulfillment;
+        this.completionActionType = QuestCompletionActionType.findByID(completionActionType);
+        try
+        {
+            this.startDate = dateFormat.parse(startDate);
+            this.endDate = dateFormat.parse(endDate);
+        }
+        catch (ParseException e)
+        {
+            // TODO: figure out how to throw error back to front end from here
+            throw new RuntimeException(e);
+        }
+        this.easterEgg = easterEgg;
+    }
+}
