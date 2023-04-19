24a11a3d1401d591f493349d369b57ced85d4194
Joelle <jg2219@engr.ship.edu>
2023-04-19 01:19:30 -0400

    edited create_edit_quest_page.dart

--- game_manager/lib/pages/quest/create_edit_quest_page.dart
+++ game_manager/lib/pages/quest/create_edit_quest_page.dart
@@ -3,21 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_manager/pages/quest/bloc/quest_bloc.dart';
 import 'package:game_manager/repository/quest/quest_editing_response.dart';
-import 'package:game_manager/repository/quest/quest_repository.dart';
 
 import '../../repository/quest/quest_record.dart';
-
+import '../../repository/quest/quest_repository.dart';
 
 class CreateEditQuestPage extends StatefulWidget {
   const CreateEditQuestPage({Key? key}) : super(key: key);
 
   @override
   State<CreateEditQuestPage> createState() => _CreateEditQuestPageState();
-
-
 }
 
-class _CreateEditQuestPageState extends State<CreateEditQuestPage>{
+class _CreateEditQuestPageState extends State<CreateEditQuestPage> {
   final experienceGained = TextEditingController();
   final questDesc = TextEditingController();
   final triggerRow = TextEditingController();
@@ -30,23 +27,24 @@ class _CreateEditQuestPageState extends State<CreateEditQuestPage>{
 
   // this is here for example purposes! take it out when you have the data
   // ready to fill in.
-  static const List<String> list = <String>['Quest Title', 'Two', 'Three',
-    'Four'];
+  static const List<String> list = <String>[
+    'Quest Title',
+    'Two',
+    'Three',
+    'Four'
+  ];
   String dropdownValue = list.first;
 
-  int? questTitle;
-  int? triggerMap;
-  int? completionActionType;
+  int? questId;
+  String? questTitle;
+  String? mapValue;
+  int? actionValue;
 
   @override
   void initState() {
     super.initState();
-
-    questTitle = 0;
-    triggerMap = 0;
-    completionActionType = 0;
+    addNewQuest.addListener(refresh);
   }
-
 
   // you'll probably have to change this when the time comes
   @override
@@ -59,7 +57,12 @@ class _CreateEditQuestPageState extends State<CreateEditQuestPage>{
     fulfillmentObjectives.dispose();
     startDate.dispose();
     endDate.dispose();
+    addNewQuest.removeListener(refresh);
+  }
 
+  // Function to sync the password visibilities by refreshing Parent state
+  refresh() {
+    setState(() {});
   }
 
   // DO NOT TAKE THIS OUT! the page will not build!
@@ -70,8 +73,8 @@ class _CreateEditQuestPageState extends State<CreateEditQuestPage>{
     ],
         child: BlocProvider<QuestBloc>(
           create: (context) => QuestBloc(
-              questRepository: context.read<QuestRepository>())
-              ..add(SendGetQuestEditingInformationEvent()),
+            questRepository: context.read<QuestRepository>())..add(
+              SendGetQuestEditingInformationEvent()),
           child: Scaffold(
             resizeToAvoidBottomInset: false,
             appBar: AppBar(
@@ -85,6 +88,7 @@ class _CreateEditQuestPageState extends State<CreateEditQuestPage>{
                 state) {
                   if(state is QuestPageReady) {
                     questResponse = state.response;
+                    print(questResponse);
                     return buildInputScreen(questResponse);
                   }
                   else {
@@ -92,20 +96,18 @@ class _CreateEditQuestPageState extends State<CreateEditQuestPage>{
                   }
                 });
               }
-            ),
-          ),
+            )
+          )
         ));
   }
 
-  Widget buildLoadScreen() =>
-      const Padding(
-          padding: EdgeInsets.all(24.0),
-          child: Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          )
-      );
+  Widget buildLoadScreen() => const Padding(
+      padding: EdgeInsets.all(24.0),
+      child: Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ));
 
   // INSTRUCTIONS/NOTES:
   // To make a new component of a page (another text field or dropdown), it
@@ -117,224 +119,305 @@ class _CreateEditQuestPageState extends State<CreateEditQuestPage>{
   // you should put the submit button at the bottom of the buildinputscreen
 
   Widget buildInputScreen(QuestResponse questResponse) => Padding(
-    padding: const EdgeInsets.all(24.0),
-    child: Center(
-      child: Column(
-        children: [
-          // start putting in your page components here
-          // QUEST TITLE
-          Row (
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: addNewQuest,
-                  decoration: InputDecoration(
-                    label: Row(
-                      children: const[
-                        Icon(Icons.edit_note, color: Colors.pink),
-                        SizedBox(
-                          width: 10,
-                          height: 10,
-                        ),
-                        Text('Type New Quest Name:'),
-                      ],
+      padding: const EdgeInsets.all(24.0),
+      child: Center(
+          child: ListView(children: [
+        // start putting in your page components here
+        // QUEST TITLE
+        Row(children: [
+          Expanded(
+            child: TextField(
+              controller: addNewQuest,
+              decoration: const InputDecoration(
+                label: Row(
+                  children: [
+                    Icon(Icons.edit_note, color: Colors.pink),
+                    SizedBox(
+                      width: 10,
+                      height: 10,
                     ),
-                    fillColor: Colors.grey,
-                  ),
+                    Text('Type New Quest Name:'),
+                  ],
                 ),
+                fillColor: Colors.grey,
               ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: DropdownButtonFormField<int>(
-                  decoration: const InputDecoration(
-                      prefixIcon: Icon(Icons.assignment, color: Colors.pink)
-                  ),
-                  hint: const Text("Quest Title"),
-                  value: questTitle,
-                  isExpanded: true,
-                  onChanged: (int? value) {
-                    setState(() {
-                      questTitle = value!;
-                    });
-                  },
-                  items: questResponse.questEditingInfoDTO.quests.map
-                  <DropdownMenuItem<int>>((QuestRecord quests) {
-                    return DropdownMenuItem<int>(
-                      value: quests.id,
-                      child: Text(quests.title),
-                    );
-                  }).toList(),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: DropdownButtonFormField<int>(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.assignment, color: Colors.pink)),
+              hint: const Text("Quest Title"),
+              value: questId,
+              isExpanded: true,
+              onChanged: (int? value) {
+                setState(() {
+                  questId = value!;
+                });
+              },
+              items: questResponse.questEditingInfoDTO.quests.map<DropdownMenuItem<int>>((QuestRecord quests) {
+                return DropdownMenuItem<int>(
+                  value: quests.id,
+                  child: Text(quests.title),
+                );
+              }).toList(),
+            ),
+          )
+        ]),
+        // QUEST DESCRIPTION:
+        TextField(
+          controller: questDesc,
+          decoration: const InputDecoration(
+            label: Row(
+              children: [
+                Icon(Icons.edit_note, color: Colors.pink),
+                SizedBox(
+                  width: 10,
+                  height: 10,
                 ),
-              )
-            ]
-          ),
-          // QUEST DESCRIPTION:
-          TextField(
-            controller: questDesc,
-            decoration:  InputDecoration(
-              label: Row(
-                children:const [
-                  Icon(Icons.edit_note, color: Colors.pink),
-                  SizedBox(
-                    width: 10,
-                    height: 10,
-                  ),
-                  Text('Quest Description...'),
-                ],
-              ),
-              fillColor: Colors.grey,
+                Text('Quest Description...'),
+              ],
             ),
+            fillColor: Colors.grey,
           ),
+        ),
 
-          // EXPERIENCE GAINED
-          TextField(
-            controller: experienceGained,
-            decoration:  InputDecoration(
-              label: Row(
-                children:const [
-                  Icon(Icons.exposure_plus_1, color: Colors.pink),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text('Experience Gained'),
-                ],
-              ),
-              fillColor: Colors.grey,
+        // EXPERIENCE GAINED
+        TextField(
+          controller: experienceGained,
+          decoration: const InputDecoration(
+            label: Row(
+              children: [
+                Icon(Icons.exposure_plus_1, color: Colors.pink),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('Experience Gained'),
+              ],
             ),
+            fillColor: Colors.grey,
           ),
+        ),
 
-          // TRIGGER MAP
-          DropdownButtonFormField<String>(
-            decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.map_outlined, color: Colors.pink)
+        // TRIGGER MAP
+        DropdownButtonFormField<String>(
+          decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.map_outlined, color: Colors.pink)),
+          hint: const Text("Trigger Map"),
+          value: dropdownValue,
+          isExpanded: true,
+          onChanged: (String? value) {
+            setState(() {
+              dropdownValue = value!;
+            });
+          },
+          items: list.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+
+        // TRIGGER ROW
+        TextField(
+          controller: triggerRow,
+          decoration: const InputDecoration(
+            label: Row(
+              children: [
+                Icon(Icons.arrow_right_alt, color: Colors.pink),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('Trigger Row'),
+              ],
             ),
-            hint: const Text("Trigger Map"),
-            value: dropdownValue,
-            isExpanded: true,
-            onChanged: (String? value) {
-              setState(() {
-                dropdownValue = value!;
-              });
-            },
-            items: list.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
+            fillColor: Colors.grey,
           ),
+        ),
 
-          // TRIGGER ROW
-          TextField(
-            controller: triggerRow,
-            decoration:  InputDecoration(
-              label: Row(
-                children:const [
-                  Icon(Icons.arrow_right_alt, color: Colors.pink),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text('Trigger Row'),
-                ],
-              ),
-              fillColor: Colors.grey,
+        // TRIGGER COLUMN
+        TextField(
+          controller: triggerColumn,
+          decoration: const InputDecoration(
+            label: Row(
+              children: [
+                Icon(Icons.arrow_right_alt, color: Colors.pink),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('Trigger Column'),
+              ],
             ),
+            fillColor: Colors.grey,
           ),
+        ),
 
-          // TRIGGER COLUMN
-          TextField(
-            controller: triggerColumn,
-            decoration:  InputDecoration(
-              label: Row(
-                children:const [
-                  Icon(Icons.arrow_right_alt, color: Colors.pink),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text('Trigger Column'),
-                ],
-              ),
-              fillColor: Colors.grey,
+        // FULFILLMENT OBJECTIVES
+        TextField(
+          controller: fulfillmentObjectives,
+          decoration: const InputDecoration(
+            label: Row(
+              children: [
+                Icon(Icons.edit_note, color: Colors.pink),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('Objectives for Fulfillment'),
+              ],
             ),
+            fillColor: Colors.grey,
           ),
+        ),
 
-          // FULFILLMENT OBJECTIVES
-          TextField(
-            controller: fulfillmentObjectives,
-            decoration:  InputDecoration(
-              label: Row(
-                children:const [
-                  Icon(Icons.edit_note, color: Colors.pink),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text('Objectives for Fulfillment'),
-                ],
-              ),
-              fillColor: Colors.grey,
+        // COMPLETION ACTION TYPE
+        DropdownButtonFormField<String>(
+          decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.incomplete_circle, color: Colors.pink)),
+          hint: const Text("Trigger Map"),
+          value: dropdownValue,
+          isExpanded: true,
+          onChanged: (String? value) {
+            setState(() {
+              dropdownValue = value!;
+            });
+          },
+          items: list.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+
+        // START DATE
+        TextField(
+          controller: startDate,
+          decoration: const InputDecoration(
+            label: Row(
+              children: [
+                Icon(Icons.calendar_today, color: Colors.pink),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('Start Date'),
+              ],
             ),
+            fillColor: Colors.grey,
           ),
+        ),
 
-          // COMPLETION ACTION TYPE
-          DropdownButtonFormField<String>(
-            decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.incomplete_circle, color: Colors.pink)
+        // END DATE
+        TextField(
+          controller: endDate,
+          decoration: const InputDecoration(
+            label: Row(
+              children: [
+                Icon(Icons.calendar_today, color: Colors.pink),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('End Date'),
+              ],
             ),
-            hint: const Text("Trigger Map"),
-            value: dropdownValue,
-            isExpanded: true,
-            onChanged: (String? value) {
-              setState(() {
-                dropdownValue = value!;
-              });
-            },
-            items: list.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
+            fillColor: Colors.grey,
           ),
-
-          // START DATE
-          TextField(
-            controller: startDate,
-            decoration:  InputDecoration(
-              label: Row(
-                children: const [
-                  Icon(Icons.calendar_today, color: Colors.pink),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text('Start Date'),
-                ],
-              ),
-              fillColor: Colors.grey,
-            ),
-          ),
-
-          // END DATE
-          TextField(
-            controller: endDate,
-            decoration:  InputDecoration(
-              label: Row(
-                children: const [
-                  Icon(Icons.calendar_today, color: Colors.pink),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text('End Date'),
-                ],
-              ),
-              fillColor: Colors.grey,
-            ),
-          ),
-
-        ]
-      )
-    )
-  );
+        ),
+        SubmitButtonBuilder(
+          questId: questId ?? -1,
+          questTitle: addNewQuest.text.isNotEmpty
+              ? addNewQuest.text
+              : questTitle,
+          questDescription:
+              questDesc.text.isNotEmpty ? questDesc.text : null,
+          xpGained: experienceGained.text.isNotEmpty
+              ? int.parse(experienceGained.text)
+              : 0,
+          triggerMapName: mapValue,
+          triggerRow:
+              triggerRow.text.isNotEmpty ? int.parse(triggerRow.text) : 0,
+          triggerColumn: triggerColumn.text.isNotEmpty
+              ? int.parse(triggerColumn.text)
+              : 0,
+          fulfillmentObjectives: fulfillmentObjectives.text.isNotEmpty
+              ? int.parse(fulfillmentObjectives.text)
+              : 0,
+          completionActionType: actionValue ?? 0,
+          startDate: startDate.text.isNotEmpty ? startDate.text : null,
+          endDate: endDate.text.isNotEmpty ? endDate.text : null,
+          isEasterEgg: false,
+          isValid: questTitle != null || addNewQuest.text.isNotEmpty,
+        )
+      ])));
 }
 
+class SubmitButtonBuilder extends StatelessWidget {
+  SubmitButtonBuilder({
+    Key? key,
+    this.questId = -1,
+    required this.questTitle,
+    this.questDescription,
+    this.xpGained = 0,
+    this.triggerMapName,
+    this.triggerRow = 0,
+    this.triggerColumn = 0,
+    this.fulfillmentObjectives = 0,
+    this.completionActionType = 0,
+    this.startDate,
+    this.endDate,
+    this.isEasterEgg = false,
+    required this.isValid,
+  }) : super(key: key);
 
-  
+  final int questId;
+  final String? questTitle;
+  final String? questDescription;
+  final int xpGained;
+  final String? triggerMapName;
+  final int triggerRow;
+  final int triggerColumn;
+  final int fulfillmentObjectives;
+  final int completionActionType;
+  final String? startDate;
+  final String? endDate;
+  final bool isEasterEgg;
+
+  bool isValid;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(1),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.blue,
+          minimumSize: const Size(double.infinity, 50),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(1)),
+          ),
+        ),
+        onPressed: !isValid
+            ? null
+            : () => BlocProvider.of<QuestBloc>(context).add(
+                SendUpsertQuestEvent(
+                    questId,
+                    questTitle,
+                    questDescription,
+                    xpGained,
+                    triggerMapName,
+                    triggerRow,
+                    triggerColumn,
+                    fulfillmentObjectives,
+                    completionActionType,
+                    startDate,
+                    endDate,
+                    isEasterEgg)),
+        child: Text(
+          (questId == -1) ? "Create Quest" : "Update Quest",
+          style: const TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
