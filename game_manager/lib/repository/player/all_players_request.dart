1062bfb9dba63b3844c71b0e568169f141eb2c6e
Badreddine Bouhmam <bb6848@engr.ship.edu>
2023-04-24 20:42:10 +0000

    Resolve "Request & response for getting all players"

---
+++ game_manager/lib/repository/player/all_players_request.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class AllPlayersRequest extends Equatable {
+
+  const AllPlayersRequest();
+
+  @override
+  List<Object?> get props => [];
+
+  Map<String, dynamic> toJson() {
+    return {};
+  }
+
+  @override
+  String toString() {
+    return 'AllPlayersRequest()';
+  }
+}
