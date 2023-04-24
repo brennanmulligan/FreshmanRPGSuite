1ea06803e218a253bf6c23935196c1f7e0fb0659
Adam Haertter <ah2005@engr.ship.edu>
2023-04-24 03:29:13 +0000

    Resolve "Add and fill dropdown with data on password page"

--- game_manager/lib/pages/change_password/change_password_page.dart
+++ game_manager/lib/pages/change_password/change_password_page.dart
@@ -5,6 +5,7 @@ import 'package:game_manager/pages/change_password/bloc/change_password_bloc.dar
 import 'package:game_manager/pages/shared/widgets/password.dart';
 
 import 'package:game_manager/repository/player/player_repository.dart';
+import '../../repository/player/all_players_response.dart';
 import '../../repository/player/basic_response.dart';
 import '../shared/widgets/notification_card.dart';
 
@@ -16,9 +17,9 @@ class ChangePasswordPage extends StatefulWidget {
 }
 
 class _ChangePasswordPageState extends State<ChangePasswordPage> {
-  final username = TextEditingController();
   final passwordFirst = TextEditingController();
   final passwordConfirm = TextEditingController();
+  String? username;
   bool isMatching = true;
   PasswordValidator validator = PasswordValidator();
 
@@ -54,94 +55,91 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
     return Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
-        title: const Text('Change Password'),
+          title: const Text('Change Password'),
         ),
-      body:
-      RepositoryProvider(
-        create: (context) => PlayerRepository(dio: Dio()),
-
-        child: BlocProvider<ChangePasswordBloc>(
-          create: (context) => ChangePasswordBloc
-            (playerRepository: context.read<PlayerRepository>()),
-
-            child:
-            BlocConsumer<ChangePasswordBloc, ChangePasswordState>(
-                listener: (context, state) {
+        body: RepositoryProvider(
+            create: (context) => PlayerRepository(dio: Dio()),
+            child: BlocProvider<ChangePasswordBloc>(
+                create: (context) => ChangePasswordBloc(
+                    playerRepository: context.read<PlayerRepository>()),
+                child: BlocConsumer<ChangePasswordBloc, ChangePasswordState>(
+                    listener: (context, state) {
                   if (state is ChangePasswordComplete) {
                     ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(
                           content: NotificationCard(
-                              cardTitle: state.response.success ? "Success" : "Error",
+                              cardTitle:
+                                  state.response.success ? "Success" : "Error",
                               description: state.response.description,
-                              success: state.response.success
-                          ),
+                              success: state.response.success),
                           duration: const Duration(seconds: 3),
                           behavior: SnackBarBehavior.floating,
-                          padding: EdgeInsets.zero
-                      ),
+                          padding: EdgeInsets.zero),
                     );
                   }
-                },
-                builder: (context, state) {
-                    return buildInputScreen();
-                })
-      )));
+                }, builder: (context, state) {
+                  if (state is PasswordPageReady) {
+                    return buildInputScreen(state.response);
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                }))));
   }
 
-  Widget buildInputScreen() => Padding(
-    padding: const EdgeInsets.all(24.0),
-    child: Center(
-      child: Column(
-        children: [
-          // TextField for Players
-          TextField(
-            controller: username,
-            decoration: InputDecoration(
-              label: Row(
-                children: const [
-                  Icon(Icons.person),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text('Player Username'),
-                ],
-              ),
-              fillColor: Colors.grey,
+  Widget buildInputScreen(AllPlayersResponse playersResponse) => Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Center(
+          child: Column(children: [
+        DropdownButtonFormField(
+          decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.person, color: Colors.black)),
+          hint: const Text("Players"),
+          value: username,
+          onChanged: (String? value) {
+            setState(() {
+              username = value!;
+            });
+          },
+          items: playersResponse.players
+              .map<DropdownMenuItem<String>>((String playerName) {
+            return DropdownMenuItem<String>(
+              value: playerName,
+              child: Text(playerName),
+            );
+          }).toList(),
+        ),
+        // Special TextField to enter new password
+        PasswordField(
+          controller: passwordFirst,
+          validator: validator,
+          notifyParent: refresh,
+        ),
+        // TextField to confirm password
+        TextField(
+          controller: passwordConfirm,
+          obscureText: !validator.passwordVisible,
+          decoration: InputDecoration(
+            label: Row(
+              children: const [
+                Icon(Icons.key),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('Confirm Password'),
+              ],
             ),
+            errorText: isMatching ? null : "The two passwords do not match.",
+            fillColor: Colors.grey,
           ),
-          // Special TextField to enter new password
-          PasswordField(
-            controller: passwordFirst,
-            validator: validator,
-            notifyParent: refresh,
-          ),
-          // TextField to confirm password
-          TextField(
-            controller: passwordConfirm,
-            obscureText: !validator.passwordVisible,
-            decoration: InputDecoration(
-              label: Row(
-                children: const [
-                  Icon(Icons.key),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text('Confirm Password'),
-                ],
-              ),
-              errorText: isMatching ? null : "The two passwords do not match.",
-              fillColor: Colors.grey,
-            ),
-          ),
-          const SizedBox(
-            height: 60,
-          ),
-          SubmitButtonBuilder(username: username,
-              password: passwordConfirm, isValid: (isMatching && validator.isSecure))
-        ]
-      )
-    )
-  );
+        ),
+        const SizedBox(
+          height: 60,
+        ),
+        SubmitButtonBuilder(
+            username: username ?? "",
+            password: passwordConfirm,
+            isValid: (isMatching && validator.isSecure && username != null))
+      ])));
 
   Widget buildRequestCompleteScreen(BasicResponse data) =>
       Center(child: Text('$data'));
@@ -150,12 +148,12 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
 class SubmitButtonBuilder extends StatelessWidget {
   const SubmitButtonBuilder(
       {Key? key,
-        required this.username,
-        required this.password,
-        required this.isValid})
+      required this.username,
+      required this.password,
+      required this.isValid})
       : super(key: key);
 
-  final TextEditingController username;
+  final String username;
   final TextEditingController password;
   final bool isValid;
 
@@ -171,8 +169,10 @@ class SubmitButtonBuilder extends StatelessWidget {
             borderRadius: BorderRadius.all(Radius.circular(1)),
           ),
         ),
-        onPressed: !isValid ? null : () => BlocProvider.of<ChangePasswordBloc>(context)
-            .add(SendChangePasswordEvent(username.text, password.text)),
+        onPressed: !isValid
+            ? null
+            : () => BlocProvider.of<ChangePasswordBloc>(context)
+                .add(SendChangePasswordEvent(username, password.text)),
         child: const Text(
           "Change Password",
           style: TextStyle(color: Colors.black),
