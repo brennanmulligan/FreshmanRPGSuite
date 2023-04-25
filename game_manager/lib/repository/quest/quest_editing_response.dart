da26780b1fac6ebcba21cddb6d9f0112701eee5d
Joelle Godwin <jg2219@engr.ship.edu>
2023-04-25 13:30:32 +0000

    Resolve "page functionality to add dropdowns to create edit quest page"

--- game_manager/lib/repository/quest/quest_editing_response.dart
+++ game_manager/lib/repository/quest/quest_editing_response.dart
@@ -1,30 +1,41 @@
 import 'package:equatable/equatable.dart';
 
 import '../../type_definitions.dart';
-import 'quest_editing_info_DTO.dart';
+import 'action_type_DTO.dart';
+import 'quest_record.dart';
 
 class QuestResponse extends Equatable {
   final bool success;
-  final QuestEditingInfoDTO questEditingInfoDTO;
+  final List<QuestRecord> quests;
+  final List<String> mapNames;
+  final List<ActionTypeDTO> completionActionTypes;
 
-  const QuestResponse(this.success, {required this.questEditingInfoDTO});
+  const QuestResponse(this.success, {required this.quests, required this.mapNames, required this.completionActionTypes});
 
-  const QuestResponse.allFields({required this.success,required this.questEditingInfoDTO});
+  const QuestResponse.allFields({required this.success,required this.quests, required this.mapNames, required this.completionActionTypes});
 
   factory QuestResponse.fromJson({
     required JSON json,
   }) {
     return QuestResponse.allFields(
       success: json['success'],
-      questEditingInfoDTO: QuestEditingInfoDTO.fromJson(json: json['questEditingInfoDTO']),
+      quests: (json['quests'] as List)
+          .map((e) => QuestRecord.fromJson(json: e))
+          .toList(),
+      mapNames: (json['mapNames'] as List)
+          .map((e) => e.toString())
+          .toList(),
+      completionActionTypes: (json['completionActionTypes'] as List)
+          .map((e) => ActionTypeDTO.fromJson(json: e))
+          .toList(),
     );
   }
 
   @override
-  List<Object> get props => [success, questEditingInfoDTO];
+  List<Object> get props => [success, quests, mapNames, completionActionTypes];
 
   @override
   String toString() {
-    return 'QuestResponse(questEditingInfoDTO: $questEditingInfoDTO)';
+    return 'QuestResponse(quests: $quests, mapNames: $mapNames, completionActionTypes: $completionActionTypes)';
   }
 }
