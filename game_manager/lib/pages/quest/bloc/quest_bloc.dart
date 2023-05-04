5414fce196f21e4f458218050e6f320ffc851ec4
Bobby Tables <ns7154@engr.ship.edu>
2023-05-04 14:58:04 -0400

    Making progress in fixing quest upsert

--- game_manager/lib/pages/quest/bloc/quest_bloc.dart
+++ game_manager/lib/pages/quest/bloc/quest_bloc.dart
@@ -26,6 +26,10 @@ class QuestBloc extends Bloc<QuestEvent, QuestState> {
       BasicResponse response = await questRepository.upsertQuest(UpsertQuestRequest(id: event.id, title: event.title, description: event.description, objectives: event.objectives, xpGained: event.xpGained, triggerMapName: event.triggerMapName, triggerRow: event.triggerRow, triggerCol: event.triggerCol, objectivesForFulfillment: event.objectivesForFulfillment, completionActionType: event.completionActionType, startDate: event.startDate, endDate: event.endDate, easterEgg: event.easterEgg));
 
       emit(QuestComplete(response));
+
+      QuestResponse quest_response = await questRepository.getQuests(const QuestRequest());
+
+      emit(QuestPageReady(quest_response));
     });
 
     on<SendGetQuestEditingInformationEvent>((event, emit) async {
